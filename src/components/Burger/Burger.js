d0deb7dc370e884f5ee0546a30412a38d1856e74
Sona Leitmanova <sona.leitmanova@gmail.com>
2018-06-01 18:41:48 +0200

    redux added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,8 +10,9 @@ const burger = (props) => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
-        }).reduce((arr, el) => {
-            return arr.concat(el);
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
         }, []); //original array of arrays turns into simple array of objects
     console.log(transformedIngredients);
     if (transformedIngredients.length === 0) {
