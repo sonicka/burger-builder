a501299471f01b6c24f5034b8a9957af56311b41
Soňa Leitmanová <sona.leitmanova@gmail.com>
2019-01-06 21:59:01 +0100

    login cont

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-import orderReducer from './store/reducers/order';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
+    auth: authReducer,
 });
 
 const store = createStore(rootReducer, composeEnhancers(
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData,
+    idToken: token,
+    userId: userId,
   };
 };
 
@@ -21,7 +22,23 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+// todo 3 input fieldy
+
+export const logOut = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logOut());
+    }, expirationTime * 1000);
+  }
+};
+
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -29,14 +46,19 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true,
     }
-    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAmvbRWlsL0KXMQ1mkBN3UPr9jbMOJuCDU', authData)
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAmvbRWlsL0KXMQ1mkBN3UPr9jbMOJuCDU';
+    if (!isSignup) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAmvbRWlsL0KXMQ1mkBN3UPr9jbMOJuCDU';
+    }
+    axios.post(url, authData)
     .then(response => {
       console.log(response);
-      dispatch(authSuccess(response.data));
+      dispatch(authSuccess(response.data.idToken, response.data.localId));
+      dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(err => {
       console.log(err);
-      dispatch(authFail(err));
+      dispatch(authFail(err.response.data.error));
     })
   };
-};
+}; 
