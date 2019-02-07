d68e7765e5d395dab48f447a7a549487f83418cd
Soňa Leitmanová <sona.leitmanova@gmail.com>
2019-02-07 22:54:48 +0100

    unit tests added

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,39 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reucer', () => {
+  it('should should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    });
+  });
+
+  it('should store a token upon login', () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: '/'
+        },
+        {
+          type: actionTypes.AUTH_SUCCESS,
+          idToken: 'some-token',
+          userId: 'some-id'
+        }
+      )
+    ).toEqual({
+      token: 'some-token',
+      userId: 'some-id',
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    });
+  });
+});
